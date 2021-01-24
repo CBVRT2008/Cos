af40999abf95969b22d546ccc9d650ba7f5dac5d
CBVRT2008 <63355565+CBVRT2008@users.noreply.github.com>
2021-01-24 18:51:14 +0100

    learn to spell

--- Cos_1.1 Crash bear/Cos/Cos64/CrashHandler.py
+++ Cos_1.1 Crash bear/Cos/Cos64/CrashHandler.py
@@ -1,7 +1,7 @@
 from Cos.Cos64 import Kernel
 
-message = "\nA fatal error has occured in the currently running prosess that caused a kernal panic\n" \
-          "This screen has occured to protect your system for further damage caused by the crashed program\n" \
+message = "\nA fatal error has occured in the currently running prosess that caused a kernel panic\n" \
+          "This screen has occured to protect your system against further damage caused by the crashed program\n" \
           "The os will be shut-down after the enter key is pressed\n\n" \
           "All unsaved data will be lost!\n\n\n" \
           "Error code:"
